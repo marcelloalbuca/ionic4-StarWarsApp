190be493e8552c1481973bc1e759f9d44e206efc
marcelloalbuca <marcello.albuca@gmail.com>
2019-09-09 19:18:57 -0300

    estrutura projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  // { path: '', redirectTo: 'tabs', pathMatch: 'full' },
+  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/films/films.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-films',
+  templateUrl: './films.page.html',
+  styleUrls: ['./films.page.scss'],
+})
+export class FilmsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
